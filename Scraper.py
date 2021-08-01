14ea3b02ae581a4cd26254d91baf4b7f04d80f9f
stefan <sdiener7@gmail.com>
2021-08-02 00:27:29 +0200

    parser works for testpage

--- Scraper.py
+++ Scraper.py
@@ -1,12 +1,10 @@
 import os
 import re
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import time
-
-
 
 def immonet_parser(page_source):
     ''' Parser extracts infos from immonet.de search results '''
@@ -17,30 +15,32 @@ def immonet_parser(page_source):
     selObjects = soup.find_all("div", {"id": re.compile("^selObject_")}) # Find all 
     print("Found {} aparments on page".format(len(selObjects)))
 
+    df = pd.DataFrame(columns=["id","title","bullets","tags","price","area","rooms"])
     for i, selObject in enumerate(selObjects):
-        print("- Extracting sell Object: {}".format(i))
+        print("- Extracting sell Object: {}/{}".format(i, len(selObjects)-1))
         id = selObject.get('id')
 
-        # extract elements
-        titles = selObject.find_all("a", {"id": re.compile("^lnkToDetails_")})
-        bullets = selObject.find_all("span", class_="text-100")      
+        # extract elements                      #TODO add error handling!
+        title = selObject.find("a", {"id": re.compile("^lnkToDetails_")})
+        title = " ".join(title.get_text().split())
+
+        bullets = selObject.find("span", class_="text-100")
+        bullets = bullets.get_text().split()
+        
         tags = selObject.find_all("span", class_="tag-element-50")
-        price = selObject.find_all("div", {"id": re.compile("^selPrice_")})
-        area = selObject.find_all("div", {"id": re.compile("^selArea_")})
-        rooms = selObject.find_all("div", {"id": re.compile("^selRooms_")})
-
-        # convert to text # TODO better way to extract relevant information? split/strip
-        title_txt = [i.get_text() for i in titles]
-        bullets_txt = [i.get_text() for i in bullets]
-        tags_txt = [i.get_text() for i in tags]
-        price_txt = [i.get_text() for i in price]
-        area_txt = [i.get_text() for i in area]
-        rooms_txt = [i.get_text() for i in rooms]
-
-        return # TODO return sth. 
-
-
-
+        tags = [t.get_text() for t in tags]
+        
+        price = selObject.find("div", {"id": re.compile("^selPrice_")})
+        price = " ".join(price.get_text().split())
+        
+        area = selObject.find("div", {"id": re.compile("^selArea_")})
+        area = " ".join(area.get_text().split())
+        
+        rooms = selObject.find("div", {"id": re.compile("^selRooms_")})
+        rooms = " ".join(rooms.get_text().split())
+        
+        df = df.append({"id":id, "title":title, "bullets":bullets, "tags":tags, "price":price, "area":area, "rooms":rooms}, ignore_index=True)
+    return df
 
 
 # Operating in headless mode
@@ -52,34 +52,5 @@ print("Loading webpage..")
 driver = webdriver.Firefox(options=opts, executable_path="geckodriver.exe")
 driver.get("https://www.immonet.de/immobiliensuche/sel.do?parentcat=1&objecttype=1&pageoffset=1&listsize=26&suchart=1&sortby=0&city=121673&marketingtype=2&page=2")
 
-# # save page to file
-# with open("soup1.html", "w", encoding='utf-8') as file:
-#     file.write(str(soup))
-
-# # load file again
-# page_source_html = open("soup1.html").read()
-
-immonet_parser(driver.page_source)
-
-# DESCRIPTION:
-# < a id="lnkToDetails_44898080" title="HELLES 3-ZI DACHGESCHOSS-LOFT / TOP AUSSTATTUNG / IM GRÜNEN"
-# - HELLES 3-ZI DACHGESCHOSS-LOFT / TOP AUSSTATTUNG / IM GRÜNEN
-
-# TAGS:
-# <div class="box-25">
-# 			<span class="tag-element-50">Balkon</span>
-# 			<span class="tag-element-50">Garten</span>
-
-# KEYFACTS: 
-# PRICE:
-# <div id="keyfacts-bar"
-# - <div id="selPrice_42828686" >
-# -- <span> 1000 €
-
-# AREA:
-# - <div id="selArea_42828686" > 
-# -- <p> 36 m²
-
-# ROOM NR:
-# - <div id="selRooms_42828686" >
-# -- <p> 2 rooms
+df = immonet_parser(driver.page_source)
+df.to_csv("immo_data.csv")
