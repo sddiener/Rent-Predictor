30276ff643453ec46e20299d947069606b25b630
stefan <sdiener7@gmail.com>
2021-08-03 00:33:16 +0200

    scraped munich apartments

--- Scraper.py
+++ Scraper.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
@@ -11,46 +12,90 @@ def immonet_parser(page_source):
 
     soup = BeautifulSoup(page_source, "html.parser")
 
-    # find divs of all apartments on page
-    selObjects = soup.find_all("div", {"id": re.compile("^selObject_")}) # Find all 
-    print("Found {} aparments on page".format(len(selObjects)))
+    selObjects = soup.find_all("div", {"id": re.compile("^selObject_")}) # Find all  apartments on page
+    print("Found {} apartments on page".format(len(selObjects)))
 
     df = pd.DataFrame(columns=["id","title","bullets","tags","price","area","rooms"])
-    for i, selObject in enumerate(selObjects):
+
+    for i, selObject in enumerate(selObjects): # extract info from apartments
         print("- Extracting sell Object: {}/{}".format(i, len(selObjects)-1))
+        
         id = selObject.get('id')
+        
+        try:
+            title = selObject.find("a", {"id": re.compile("^lnkToDetails_")})
+            title = " ".join(title.get_text().split())
+        except Exception as e:
+            print("Error extracting title: {}".format(e))
 
-        # extract elements                      #TODO add error handling!
-        title = selObject.find("a", {"id": re.compile("^lnkToDetails_")})
-        title = " ".join(title.get_text().split())
+        try:
+            bullets = selObject.find("span", class_="text-100")
+            bullets = bullets.get_text().split()
+        except Exception as e:
+            print("Error extracting bullets: {}".format(e))
 
-        bullets = selObject.find("span", class_="text-100")
-        bullets = bullets.get_text().split()
+        try:
+            tags = selObject.find_all("span", class_="tag-element-50")
+            tags = [t.get_text() for t in tags]
+        except Exception as e:
+            print("Error extracting tags: {}".format(e))
+
+        try:
+            price = selObject.find("div", {"id": re.compile("^selPrice_")})
+            price = " ".join(price.get_text().split())
+        except Exception as e:
+            print("Error extracting price: {}".format(e))
+
+        try:
+            area = selObject.find("div", {"id": re.compile("^selArea_")})
+            area = " ".join(area.get_text().split())
+        except Exception as e:
+            print("Error extracting area: {}".format(e))
         
-        tags = selObject.find_all("span", class_="tag-element-50")
-        tags = [t.get_text() for t in tags]
-        
-        price = selObject.find("div", {"id": re.compile("^selPrice_")})
-        price = " ".join(price.get_text().split())
-        
-        area = selObject.find("div", {"id": re.compile("^selArea_")})
-        area = " ".join(area.get_text().split())
-        
-        rooms = selObject.find("div", {"id": re.compile("^selRooms_")})
-        rooms = " ".join(rooms.get_text().split())
-        
+        try:
+            rooms = selObject.find("div", {"id": re.compile("^selRooms_")})
+            rooms = " ".join(rooms.get_text().split())
+        except Exception as e:
+            print("Error extracting rooms: {}".format(e))
+
         df = df.append({"id":id, "title":title, "bullets":bullets, "tags":tags, "price":price, "area":area, "rooms":rooms}, ignore_index=True)
+        
     return df
 
 
-# Operating in headless mode
+# Initialize browser in headless mode
 opts = Options()
 opts.headless = False
+driver = webdriver.Firefox(options=opts, executable_path="geckodriver.exe") 
 
-# Try these websites: https://trusted.de/immoscout24-alternativen
-print("Loading webpage..")
-driver = webdriver.Firefox(options=opts, executable_path="geckodriver.exe")
-driver.get("https://www.immonet.de/immobiliensuche/sel.do?parentcat=1&objecttype=1&pageoffset=1&listsize=26&suchart=1&sortby=0&city=121673&marketingtype=2&page=2")
+# Immonet city codes
+city = ["121673", "87372",  "109447",  "113144", "105043",    "143262",   "100207",     "99990",    "102157", "116172"]
+       # München, # Berlin, # Hamburg, # Köln,   # Frankfurt, # Stuttgart, # Düsseldorf, # Dortmund, # Essen, # Leipzig 
 
-df = immonet_parser(driver.page_source)
-df.to_csv("immo_data.csv")
+# TODO
+# 1) Federal state 1-16 durchgehen!!! :D
+# for state in range(16):
+#     url = "https://www.immonet.de/immobiliensuche/sel.do?suchart=1&state=1&marketingtype=2&pageoffset=1&"\
+#           "parentcat=1&sortby=19&listsize=26&objecttype=1&federalstate={}&page={}".format(state,page)
+# 2) Weniger print statements
+# - nur pages + link / selObj nummer bei error
+# 3) Weniger CSV dateien speichern. zb nur alle 10 seiten oder so.
+
+page = 0
+while True:
+    page +=1
+    url = "https://www.immonet.de/immobiliensuche/sel.do?parentcat=1&objecttype=1&"\
+          "pageoffset=1053&listsize=27&suchart=1&sortby=0&city={}&marketingtype=2&page={}".format(city[0],page)
+    
+    print("Load website. City: {}, Page: {} \n URL: {} \n".format(city,page,url))
+    driver.get(url)
+
+    df = immonet_parser(driver.page_source)
+
+    if df.empty: # stops loop after all pages
+        break
+
+    df.to_csv("data/immo_data_city{}_page{}.csv".format(city,page))
+
+    print("Saved data.\n\n")
+    time.sleep(5)
