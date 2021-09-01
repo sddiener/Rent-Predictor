1a1f9d677db571842d72a47e297c7d28faece411
stefan <sdiener7@gmail.com>
2021-09-01 20:04:55 +0200

    created svm

--- 04_modelling.py
+++ 04_modelling.py
@@ -9,6 +9,9 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+import xgboost as xgb
+from xgboost import XGBRegressor
 
 
 def load_data(path):
@@ -76,24 +79,24 @@ if __name__ == '__main__':
     X, y = scale_and_onehot_encode(data)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
-    # Train Linear Regression
-    print("Training OLS...")
-    model_ols = LinearRegression()
-    model_ols = model_ols.fit(X_train, y_train)
-    eval_model(model_ols, X_test, y_test)
+    # # Train Linear Regression
+    # print("Training OLS...")
+    # model_ols = LinearRegression()
+    # model_ols = model_ols.fit(X_train, y_train)
+    # eval_model(model_ols, X_test, y_test)
 
-    with open('models/ols.pkl', 'wb') as f:
-        pkl.dump(model_ols, f)
+    # with open('models/ols.pkl', 'wb') as f:
+    #     pkl.dump(model_ols, f)
 
-    # Train Linear Regression
-    params_ridge = {'alpha': [int(x) for x in np.linspace(0.01, 20, num=20)]}
-    model_ridge = search_best_model(Ridge(), params_ridge, X_train, y_train, n_iter=50, cv=5)
-    eval_model(model_ridge, X_test, y_test)
+    # # Train Linear Regression
+    # params_ridge = {'alpha': [int(x) for x in np.linspace(0.01, 20, num=20)]}
+    # model_ridge = search_best_model(Ridge(), params_ridge, X_train, y_train, n_iter=50, cv=5)
+    # eval_model(model_ridge, X_test, y_test)
 
-    with open('models/ridge.pkl', 'wb') as f:
-        pkl.dump(model_ridge, f)
+    # with open('models/ridge.pkl', 'wb') as f:
+    #     pkl.dump(model_ridge, f)
 
-    # Train Random Forest
+    # # Train Random Forest
     # params_rf = {
     #     'bootstrap': [True, False],  # Method of selecting samples for training each tree
     #     'max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None],  # Max nr of levels in tree
@@ -108,3 +111,35 @@ if __name__ == '__main__':
 
     # with open('models/randomforest2.pkl', 'wb') as f:
     #     pkl.dump(model_rf, f)
+
+    # Train XGB
+
+    # params_xgb = {
+    #     'objective': ['reg:squarederror'],
+    #     'booster': ['gbtree', 'gblinear'],
+    #     'learning_rate': [0.1],
+    #     'max_depth': [7, 10, 15, 20],
+    #     'subsample': [0.6, 0.8, 1.0],
+    #     'min_child_weight': [10, 15, 20, 25],
+    #     'colsample_bytree': [0.8, 0.9, 1],
+    #     'n_estimators': [300, 500, 700, 1000, 1500, 2000],
+    #     "reg_alpha": [0.1, 0.5, 0.2, 1],
+    #     "reg_lambda": [2, 3, 5],
+    #     "gamma": [0, 1, 2, 3]}
+
+    # model_xgb = search_best_model(XGBRegressor(),
+    #                               params_xgb, X_train, y_train, n_iter=200, cv=5, verbose=0)
+    # eval_model(model_xgb, X_test, y_test)
+
+    # with open('models/xgb.pkl', 'wb') as f:
+    #     pkl.dump(model_xgb, f)
+
+    # Train SVC
+    params_svm = {'kernel': ('linear', 'rbf'),
+                  'C': [1, 10, 100]}
+
+    model_svm = search_best_model(SVR(), params_svm, X_train, y_train, n_iter=6, cv=5, verbose=2)
+    eval_model(model_svm, X_test, y_test)
+
+    with open('models/svm.pkl', 'wb') as f:
+        pkl.dump(model_svm, f)
